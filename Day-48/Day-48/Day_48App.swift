1dee7a13d4881623442054b04844dc1e8ded0775
ranmyfriend <ranjithkumar2010a@live.com>
2020-09-26 23:06:45 +0530

    Day 48 completed

---
+++ Day-48/Day-48/Day_48App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_48App.swift
+//  Day-48
+//
+//  Created by Ranjit on 26/09/20.
+//
+
+import SwiftUI
+
+@main
+struct Day_48App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
