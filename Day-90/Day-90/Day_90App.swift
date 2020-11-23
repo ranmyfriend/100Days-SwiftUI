fe20c52255f912c124c2737e0620c218f5ed8057
ranmyfriend <ranjithkumar2010a@live.com>
2020-11-23 20:33:14 +0530

    Day-90 Project 17, part 5 completed

---
+++ Day-90/Day-90/Day_90App.swift
@@ -0,0 +1,17 @@
+//
+//  Day_90App.swift
+//  Day-90
+//
+//  Created by Ranjit on 23/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Day_90App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Day-90/Day-90/EditCards.swift
@@ -0,0 +1,87 @@
+//
+//  EditCards.swift
+//  Day-90
+//
+//  Created by Ranjit on 23/11/20.
+//
+
+import SwiftUI
+
+struct EditCards: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var cards = [Card]()
+    @State private var newPrompt = ""
+    @State private var newAnswer = ""
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("Add new card")) {
+                    TextField("Prompt", text: $newPrompt)
+                    TextField("Answer", text: $newAnswer)
+                    Button("Add card", action: addCard)
+                }
+                
+                Section {
+                    ForEach(0..<cards.count, id:\.self) {
+                        index in
+                        VStack(alignment: .leading) {
+                            Text(self.cards[index].prompt)
+                                .font(.headline)
+                            Text(self.cards[index].answer)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    .onDelete(perform: removeCards)
+                }
+            }
+            
+            .navigationBarTitle("Edit Cards")
+            .navigationBarItems(trailing: Button("Done", action: dismiss))
+            .listStyle(GroupedListStyle())
+            .onAppear(perform: loadData)
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+    
+    func dismiss() {
+        presentationMode.wrappedValue.dismiss()
+    }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                self.cards = decoded
+            }
+        }
+    }
+    
+    func saveData() {
+        if let data = try? JSONEncoder().encode(cards) {
+            UserDefaults.standard.set(data, forKey: "Cards")
+        }
+    }
+    
+    func addCard() {
+        let trimmedPrompt = newPrompt.trimmingCharacters(in: .whitespaces)
+        let trimmerAnswer = newAnswer.trimmingCharacters(in: .whitespaces)
+        
+        guard trimmedPrompt.isEmpty == false && trimmerAnswer.isEmpty == false else {
+            return
+        }
+        
+        let card = Card(prompt: trimmedPrompt, answer: trimmerAnswer)
+        cards.insert(card, at: 0)
+        saveData()
+    }
+    
+    func removeCards(at offsets: IndexSet) {
+        cards.remove(atOffsets: offsets)
+        saveData()
+    }
+}
+
+struct EditCards_Previews: PreviewProvider {
+    static var previews: some View {
+        EditCards()
+    }
+}
